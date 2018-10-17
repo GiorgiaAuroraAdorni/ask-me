38c968f700e811a8f4523166fdc42857963ba06d
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2018-10-17 17:34:02 +0200

    Implemented the request for the recovery of a single question and a single answer

--- api/routes.py
+++ api/routes.py
@@ -13,3 +13,15 @@ def list_questions():
     questions = [ q.to_dict() for q in Question.query.all() ]
 
     return jsonify(questions)
+
+
+@app.route('/questions/<int:id>')
+def get_question(id):
+    from models import Question
+
+    return jsonify(Question.query.get_or_404(id).to_dict())
+@app.route('/answers/<int:id>')
+def get_answer(id):
+    from models import Answer
+
+    return jsonify(Answer.query.get_or_404(id).to_dict())
