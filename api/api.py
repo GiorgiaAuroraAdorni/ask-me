f78a22c546ede5c344fb1f89b749f336c3bbe4a1
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2018-10-18 19:39:43 +0200

    Added the possibility to see in the terminal the queries that are executed by SQLAlchemy

--- api/api.py
+++ api/api.py
@@ -1,5 +1,9 @@
 from app import create_app
 
+import logging
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
 app = create_app()
 
 if __name__ == '__main__':
