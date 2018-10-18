a5426119b6a8e54f1f543cbfaaa4df0cef6d44f9
Elia Cereda <eliacereda@gmail.com>
2018-10-18 21:36:39 +0200

    Started working on the web frontend by implementing a component that loads the list of questions

--- web/src/App.js
+++ web/src/App.js
@@ -1,28 +1,66 @@
+import axios from 'axios';
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null,
+      questions: null,
+    };
+  }
+
+  componentDidMount() {
+    this.loadQuestions();
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  async loadQuestions() {
+    const response = await axios.get('http://localhost:5000/questions');
+
+    this.setState({ questions: response.data });
+  }
+
   render() {
+    let questions;
+
+    if (this.state.questions === null) {
+      questions = "Loading…";
+    } else {
+      questions = <QuestionList questions={this.state.questions} />;
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. Ciao Puppy :D
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>AskMe</h1>
+        {questions}
       </div>
     );
   }
 }
 
 export default App;
+
+class QuestionList extends Component {
+  render() {
+    return this.props.questions.map((question) =>
+      <Question key={question.id} {...question} />
+    );
+  }
+}
+
+class Question extends Component {
+  render() {
+    return (
+        <div className="Question">
+          <h2>{this.props.title} by {this.props.user}</h2>
+          <div>{this.props.body}</div>
+        </div>
+    );
+  }
+}
