0d21d13b1bd31b1d738b3fc4cd64c82dcce1144e
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2018-10-28 21:52:28 +0100

    Addedd snapshot test for QuestionList

---
+++ web/src/QuestionList.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import QuestionList from "./QuestionList";
+
+describe('QuestionList', () => {
+    it('renders correctly', () => {
+        const questions = [
+            {
+                id: 1,
+                title: 'titolo della domanda',
+                body: 'corpo della domanda',
+                user: 'utente',
+                _links: {self: 'questions/1'}
+            },
+            {
+                id: 2,
+                title: 'titolo di un\'altra domanda',
+                body: 'corpo di un\'altra domanda',
+                user: 'anonimo',
+                _links: {self: 'questions/2'}
+            },
+            {
+                id: 2,
+                title: 'un titolo',
+                body: 'un po di testo',
+                user: 'qualcuno',
+                _links: {self: 'questions/3'}
+            },
+        ];
+
+        const wrapper = shallow(<QuestionList questions={questions} />);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
